4a25c1d316323aa5edf347574d63021cadf71cdd
salvadorripoll <140025657+salvadorripoll@users.noreply.github.com>
2023-08-28 22:51:48 -0300

    solucion-click added

--- eventos.js
+++ eventos.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-var Div = document.getElementsByTagName("div");
-for (var i = 0; i < Div.length; i++) {
-    Div[i].addEventListener("click", Hola);
-} 
+    var Div = document.getElementsByTagName("div");
+    var Button = document.getElementsByTagName("button")
+
+    for (var i = 0; i < Div.length; i++) {
+        Div[i].addEventListener("click", Hola);
+    }
+    for (var i = 0; i < Button.length; i++) {
+        Button[i].addEventListener("click", Alert);
+    }
+
 })
 
 function Hola() {
     alert("Hola! Soy el div")
+}
+
+function Alert() {
+    alert("Hola!");
+    event.stopPropagation();
 }
